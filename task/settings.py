c3c10a79589184fda04f22ba8d91d989d7499b4e
Dan Sclearov <dansclearov@gmail.com>
2021-08-12 14:20:46 +0300

    added new app "todo"

--- task/settings.py
+++ task/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -80,7 +81,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'db',
-        'PORT': '',
+        'PORT': 5432,
     }
 }
 
